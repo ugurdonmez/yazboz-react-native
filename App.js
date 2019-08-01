671ecf408d8c6ba99bb31e536b6dc726620077c8
Ugur Donmez <ugurdonmez87@gmail.com>
2019-08-01 22:31:15 +0200

    add reducer

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
 import { COLOR, ThemeContext, getTheme } from 'react-native-material-ui';
 import Container from './ScreenContainer';
 import reducer from './src/store/reducer';
 import { Provider } from 'react-redux'
 
-
-const store = createStore(reducer); 
+const store = createStore(reducer);
 
 const uiTheme = {
   palette: {
@@ -18,13 +17,14 @@ const uiTheme = {
   },
 };
 
-
 export default class App extends Component<{}> {
   render() {
     return (
-      <ThemeContext.Provider value={getTheme(uiTheme)}>
-        <Container />
-      </ThemeContext.Provider>
+      <Provider store={store}>
+        <ThemeContext.Provider value={getTheme(uiTheme)}>
+          <Container />
+        </ThemeContext.Provider>
+      </Provider>
     );
   }
 }
